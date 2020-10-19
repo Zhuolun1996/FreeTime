a8a9824747357c65a205d508076e9f0337fdb1ee
Zhuolun Li <zhuolun1996@gmail.com>
2020-10-19 00:58:32 -0700

    use mvn tomcat

--- Docker/Application/run.sh
+++ Docker/Application/run.sh
@@ -1,12 +1,9 @@
-cd /home/lizhuolu/Workspace/FreeTime/FreeTime
-mvn clean install
-
 container_name=freetime_instance
 
 if [ "$( docker container inspect -f '{{.State.Running}}' $container_name )" == "true" ]; then
 	echo "container running, patching"
-	docker exec -d $container_name /bin/bash \"jar -xf FreeTime-0.0.1-SNAPSHOT.jar && java org.springframework.boot.loader.JarLauncher\"
+	docker restart $container_name
 else
 	echo "container not running, creating container"
-	docker run -d --name $container_name --network freetime --hostname application.com --ip 192.168.0.3 -v /home/lizhuolu/Workspace/FreeTime/FreeTime/target:/usr/local/Application/release --entrypoint /bin/bash -p 8080:8080 -p 8443:8443 freetime -c "jar -xf FreeTime-0.0.1-SNAPSHOT.jar && java org.springframework.boot.loader.JarLauncher"
+	docker run -d --name $container_name --network freetime --hostname application.com --ip 192.168.0.3 -v /Users/zhuolunli/Documents/Workspace/FreeTimeApp/FreeTime/target:/usr/local/Application/release -p 8080:8080 -p 8443:8443 --entrypoint mvn freetime sprintboot:run
 fi
--- FreeTime/src/main/java/com/zhuolunutil/tool/FreeTime/FreeTimeApplication.java
+++ FreeTime/src/main/java/com/zhuolunutil/tool/FreeTime/FreeTimeApplication.java
@@ -2,11 +2,6 @@ package com.zhuolunutil.tool.FreeTime;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.client.RestTemplate;
-
-import javax.net.ssl.SSLContext;
 
 @SpringBootApplication
 public class FreeTimeApplication {
