387182571aa80ef0a9326ea9862fdc5c4740c631
Zhuolun Li <zhuolun1996@gmail.com>
2020-10-21 01:21:05 +0000

    fix use tomcat docker

--- Docker/Application/run.sh
+++ Docker/Application/run.sh
@@ -5,5 +5,5 @@ if [ "$( docker container inspect -f '{{.State.Running}}' $container_name )" == 
 	docker restart $container_name
 else
 	echo "container not running, creating container"
-	docker run -d --name $container_name --network freetime --hostname application.com --ip 192.168.0.3 -v /home/lizhuolu/Workspace/FreeTime/FreeTime:/usr/local/Application -p 8080:8080 -p 8443:8443 --entrypoint ./mvnw freetime springboot:run
+	docker run -d --name $container_name --network freetime --hostname application.com --ip 192.168.0.3 -v /home/lizhuolu/Workspace/FreeTime/FreeTime:/usr/local/Application -p 8080:8080 -p 8443:8443 --entrypoint ./mvnw freetime spring-boot:run
 fi
